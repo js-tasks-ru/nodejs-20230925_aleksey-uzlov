5ad38a3a49f06904f1f87ad3a1a7140dc037d7c0
Alexey Uzlov <auzlov@codemastersintl.com>
2023-10-07 18:32:42 +0400

    added file-server-get homework

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,17 +1,29 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      if (isNestedFile(pathname)) {
+        res.statusCode = 400;
+        res.end('Nested paths are not supported');
+      }
+
+      const stream = fs.createReadStream(filepath);
+      stream.pipe(res);
+
+      stream.on('error', () => {
+        res.statusCode = 404;
+        res.end('Not found');
+      })
 
       break;
 
@@ -20,5 +32,13 @@ server.on('request', (req, res) => {
       res.end('Not implemented');
   }
 });
+
+/**
+ * @param pathname {string}
+ * @returns {boolean}
+ */
+function isNestedFile(pathname) {
+  return pathname.includes('/');
+}
 
 module.exports = server;

cdbd8a65f0e39f236790628ae94cf97728eabf7f
Alexey Uzlov <auzlov@codemastersintl.com>
2023-10-25 20:48:47 +0400

    added search for products

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,7 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const products = await Product.find({$text: {$search: ctx.query.query}});
+  ctx.body = {products: products.map(mapProduct)};
 };

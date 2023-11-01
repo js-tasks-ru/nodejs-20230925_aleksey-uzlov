80e62d0f547b527b3b2116472185014670ba53b4
Alexey Uzlov <auzlov@codemastersintl.com>
2023-11-01 18:56:39 +0400

    added auth via local strategy

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,20 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async (email, password, done) => {
+      const user = await User.findOne({email});
+
+      if (!user) {
+        return done(null, false, 'Нет такого пользователя');
+      }
+
+      const isPasswordCorrect = await user.checkPassword(password);
+      if (!isPasswordCorrect) {
+        return done(null, false, 'Неверный пароль');
+      }
+
+      return done(null, user);
     },
 );

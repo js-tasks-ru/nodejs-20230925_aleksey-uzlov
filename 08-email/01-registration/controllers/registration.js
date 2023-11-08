f7dcba50d1e916e3e020ba6ca6dd29935740154f
Alexey Uzlov <auzlov@codemastersintl.com>
2023-11-08 12:57:31 +0400

    added registration

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -3,9 +3,32 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const { email, displayName, password } = ctx.request.body;
+  const verificationToken = uuid();
+  const user = new User({ email, displayName, verificationToken });
+  await user.setPassword(password);
+  await user.save();
 
+  await sendMail({
+    template: 'confirmation',
+    locals: { token: verificationToken },
+    to: email,
+    subject: 'Подтвердите почту',
+  });
+
+  ctx.body = { status: 'ok' };
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const { verificationToken } = ctx.request.body;
+  const user = await User.findOne({ verificationToken });
+  if (!user) {
+    ctx.throw(400, 'Ссылка подтверждения недействительна или устарела');
+  }
 
+  user.verificationToken = undefined;
+  await user.save();
+
+  const token = await ctx.login(user);
+  ctx.body = { token };
 };
